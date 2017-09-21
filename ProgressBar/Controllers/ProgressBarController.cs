39bc3f11c128bc534de393ba8121026b0a75894c
cynet sylol <bamherst@gmx.at>
2017-09-21 12:11:06 +0200

    projekt in den git/repo hineinkopiert

---
+++ ProgressBar/Controllers/ProgressBarController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProgressBar.Controllers
+{
+    public class ProgressBarController : Controller
+    {
+        // GET: ProgressBar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Button
+        public JsonResult Randomized()
+        {
+            Random rnd = new Random();
+            Session["zahl"] = rnd.Next(1, 101);
+            return Json(Session["zahl"], JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Plus(int pbcurrentvalue)
+        {
+            pbcurrentvalue ++;
+            //Session["zahl"]= Convert.ToInt32(Session["zahl"])+5;
+            //return Json(Session["zahl"], JsonRequestBehavior.AllowGet);
+            return Json(pbcurrentvalue, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Minus(int pbcurrentvalue)
+        {
+            pbcurrentvalue--;
+            //Session["zahl"] = Convert.ToInt32(Session["zahl"]) - 5;
+            //return Json(Session["zahl"], JsonRequestBehavior.AllowGet);
+            return Json(pbcurrentvalue, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Mute()
+        {
+            //pbcurrentvalue = 0;
+            Session["zahl"] = 0;
+                //Convert.ToInt32(Session["zahl"]) - Convert.ToInt32(Session["zahl"]);
+            return Json(Session["zahl"], JsonRequestBehavior.AllowGet);
+            //return Json(pbcurrentvalue, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
